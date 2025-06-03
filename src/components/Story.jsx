ea2699659b0deee7c6773d91d83680975b5392ac
khushboo-590 <khushbook4979@gmail.com>
2025-06-03 12:54:50 +0530

    add meta tag

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -7,8 +7,7 @@ const Story = () => {
         <section id="story" className="bg-[#1C2430] text-white pt-12">
             <div className="max-w-[1140px] mx-auto px-4">
                 <div className="flex justify-center items-center ">
-                    <Customheading heading="Story" />
-                </div>
+                    <Customheading heading="Story" /></div>
                 <div className="max-w-[888px] mx-auto pt-6">
                     <p className="text-center text-[#D4E5FF] ff-2 text-base leading-[21px] ">
                         Nekozuma which means lightning cats in Japanese (Neko = Cats // Inazuma = Lightning) is a collection of 5555 NFTs on the Solana blockchain. The story centers around 3 eras of cats living in different timelines - past, present, future. In
@@ -18,11 +17,9 @@ const Story = () => {
                         <br />
                         <span className="text-sm leading-[18px]">
                             However at every junction in time, there is always a darkness that is waiting to overthrow the good. Hence, each timeline has its own arch nemesis. This means that there will be a total of 4 godly nekos from each timeline making a total of 12 godly nekos that will be fully animated.
-                        </span>
-                    </p>
-                </div>
+                        </span></p></div>
             </div>
-            <div className="flex  justify-center mt-[58px]">
+            <div className="flex justify-center mt-[58px]">
                 <img src={lionImg} className="w-full  object-cover h-[200px] md:h-[250px] lg:h-[357px]" alt="lion-img" />
             </div>
         </section>
