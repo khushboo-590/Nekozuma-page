8db586c15d9397b03a6c383394ec1f2a8567eca6
khushboo-590 <khushbook4979@gmail.com>
2025-06-04 16:33:24 +0530

    check build error4

--- src/components/common/Customheading.jsx
+++ src/components/common/Customheading.jsx
@@ -1,7 +1,7 @@
 const CustomHeading = ({ title, myClass }) => {
   return (
     <div>
-      <h4 className={`font-family-franklin text-[43px] leading-[49px] font-normal text-[#3586FF] ${myClass}`}>
+      <h4 className="font-family-franklin text-[43px] leading-[49px] font-normal text-[#3586FF">
         {title}
       </h4>
     </div>
