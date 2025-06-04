84106cec62a034fb2b2ef586cea8f0ec69f3b107
khushboo-590 <khushbook4979@gmail.com>
2025-06-04 16:21:24 +0530

    check build error

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -9,7 +9,8 @@ const Team = () => {
             <div className="container mx-auto py-[30px] lg:py-[61px] max-w-[888px]">
                 <div className="flex items-center justify-center flex-col ">
                     <img src={line1} alt="line" className='mb-4 md:mb-8 lg:mb-14'/>
-                    <CustomHeading title="Team"/>
+                    {/* <CustomHeading title="Team"/> */}
+                    <CustomHeading title={"team"}/>
                     <div className="flex flex-wrap justify-center gap-2 lg:gap-4 mx-auto">
                         {teamData.map((item, index) => (
                             <div key={index} className="p-1">
